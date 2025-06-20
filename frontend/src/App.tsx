7802a0474f0f05287b5dd56ed8e4c793ded2c256
CERBERUSCHAIN <tonyjames.staub@gmail.com>
2025-06-20 16:04:08 -0500

    Create Frontend Directory Structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🐺 Cerberus Chain: Hydra</h1>
+        <p>
+          The future of memecoin trading is here.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
